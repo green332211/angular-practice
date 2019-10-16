a28e2a1d454cc1bd3b6ef82f1a285843b8e37237
green332211 <lobachtolyan123@gmail.com>
2019-10-16 20:51:25 +0300

    тренировка компонентов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'Dynamic title'
+  number = 42
+  arr = [1, 2, 3]
+
+  obj = { a: 1, b: {c: 2} }
+
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSISAPF8vrQ6xvb0zExOUS00ZB_KFeIi2UiC5ZkWvWzveIO0wATsQ'
+
+  constructor() {
+    setTimeout( () => {
+      console.log('Timeout is over')
+      this.img = 'https://angular.io/assets/images/logos/angular/angular.png'
+    }, 5000)
+  }
+
 }
